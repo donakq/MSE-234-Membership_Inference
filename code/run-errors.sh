474942b30a8c833ccb138210cbf9379501c3125b
Xingzi <xingzix@stanford.edu>
2021-03-11 16:05:01 -0800

    run sh

--- code/run-errors.sh
+++ code/run-errors.sh
@@ -1,9 +1,45 @@
-for depth in {1..30}; do
-  echo "depth = $depth"
-  time python main.py iwpc tree $depth
-done
+#for depth in {1..30}; do
+#  echo "depth = $depth"
+#  time python main.py iwpc tree $depth
+#done
+#
+#for lambda in 5.0 10.0 20.0 50.0 100.0 200.0 500.0 1000.0 2000.0 5000.0; do
+#  echo "lambda = $lambda"
+#  time python main.py eyedata linreg $lambda
+#done
 
-for lambda in 5.0 10.0 20.0 50.0 100.0 200.0 500.0 1000.0 2000.0 5000.0; do
+#lasso
+
+#for lambda in 0.0001 0.001 0.01 0.1 1.0 10.0 100.0; do
+#  echo "lambda = $lambda"
+#  time python main.py eyedata lasso $lambda
+#done
+#
+#for lambda in 0.0001 0.001 0.01 0.1 1.0 10.0 100.0; do
+#  echo "lambda = $lambda"
+#  time python main.py iwpc lasso $lambda
+#done
+
+#svm
+#
+#for C in 0.0001 0.001 0.01 0.1 1.0 10.0 100.0 1000.0; do
+#  echo "C = $C"
+#  time python main.py eyedata svm $C
+#done
+#
+#for C in 0.0001 0.001 0.01 0.1 1.0 10.0 100.0 1000.0; do
+#  echo "C = $C"
+#  time python main.py iwpc svm $C
+#done
+
+# SGDReg
+
+#for lambda in 0.0001 0.001 0.01 0.1 1.0 10.0 100.0; do
+#  echo "lambda = $lambda"
+#  time python main.py eyedata SGDReg $lambda
+#done
+
+for lambda in 0.0001 0.001 0.01 0.1 1.0 10.0 100.0; do
   echo "lambda = $lambda"
-  time python main.py eyedata linreg $lambda
+  time python main.py iwpc SGDReg $lambda
 done
--- code/run-membership.sh
+++ code/run-membership.sh
@@ -1,17 +1,74 @@
-cat ../results-sklearn/iwpc/tree-errors.txt | while read line; do
+#cat ../results-sklearn/iwpc/tree-errors.txt | while read line; do
+#  set $line
+#  echo "depth = $1"
+#  time python main.py iwpc tree $1 --inc $2 $3 --one-error
+#  time python main.py iwpc tree $1 --inc $2 $3
+#done
+#python summarize.py ../results-sklearn/iwpc/membership/unknown-test-error tree
+#python summarize.py ../results-sklearn/iwpc/membership/known-test-error tree
+#
+#cat ../results-sklearn/eyedata/linreg-errors.txt | while read line; do
+#  set $line
+#  echo "lambda = $1"
+#  time python main.py eyedata linreg $1 --inc $2 $3 --one-error
+#  time python main.py eyedata linreg $1 --inc $2 $3
+#done
+#python summarize.py ../results-sklearn/eyedata/membership/unknown-test-error linreg
+#python summarize.py ../results-sklearn/eyedata/membership/known-test-error linreg
+
+#  lasso
+cat ../results-sklearn/eyedata/lasso-errors.txt | while read line; do
   set $line
   echo "depth = $1"
-  time python main.py iwpc tree $1 --inc $2 $3 --one-error
-  time python main.py iwpc tree $1 --inc $2 $3
+  time python main.py eyedata lasso $1 --inc $2 $3 --one-error
+  time python main.py eyedata lasso $1 --inc $2 $3
 done
-python summarize.py ../results-sklearn/iwpc/membership/unknown-test-error tree
-python summarize.py ../results-sklearn/iwpc/membership/known-test-error tree
+python summarize.py ../results-sklearn/eyedata/membership/unknown-test-error lasso
+python summarize.py ../results-sklearn/eyedata/membership/known-test-error lasso
 
-cat ../results-sklearn/eyedata/linreg-errors.txt | while read line; do
+cat ../results-sklearn/iwpc/lasso-errors.txt | while read line; do
   set $line
-  echo "lambda = $1"
-  time python main.py eyedata linreg $1 --inc $2 $3 --one-error
-  time python main.py eyedata linreg $1 --inc $2 $3
+  echo "depth = $1"
+  time python main.py iwpc lasso $1 --inc $2 $3 --one-error
+  time python main.py iwpc lasso $1 --inc $2 $3
 done
-python summarize.py ../results-sklearn/eyedata/membership/unknown-test-error linreg
-python summarize.py ../results-sklearn/eyedata/membership/known-test-error linreg
+python summarize.py ../results-sklearn/iwpc/membership/unknown-test-error lasso
+python summarize.py ../results-sklearn/iwpc/membership/known-test-error lasso
+
+# svm
+cat ../results-sklearn/eyedata/svm-errors.txt | while read line; do
+  set $line
+  echo "depth = $1"
+  time python main.py eyedata svm $1 --inc $2 $3 --one-error
+  time python main.py eyedata svm $1 --inc $2 $3
+done
+python summarize.py ../results-sklearn/eyedata/membership/unknown-test-error svm
+python summarize.py ../results-sklearn/eyedata/membership/known-test-error svm
+
+cat ../results-sklearn/iwpc/svm-errors.txt | while read line; do
+  set $line
+  echo "depth = $1"
+  time python main.py iwpc svm $1 --inc $2 $3 --one-error
+  time python main.py iwpc svm $1 --inc $2 $3
+done
+python summarize.py ../results-sklearn/iwpc/membership/unknown-test-error svm
+python summarize.py ../results-sklearn/iwpc/membership/known-test-error svm
+
+# SGDReg
+cat ../results-sklearn/eyedata/SGDReg-errors.txt | while read line; do
+  set $line
+  echo "depth = $1"
+  time python main.py eyedata SGDReg $1 --inc $2 $3 --one-error
+  time python main.py eyedata SGDReg $1 --inc $2 $3
+done
+python summarize.py ../results-sklearn/eyedata/membership/unknown-test-error SGDReg
+python summarize.py ../results-sklearn/eyedata/membership/known-test-error SGDReg
+
+cat ../results-sklearn/iwpc/SGDReg-errors.txt | while read line; do
+  set $line
+  echo "depth = $1"
+  time python main.py iwpc SGDReg $1 --inc $2 $3 --one-error
+  time python main.py iwpc SGDReg $1 --inc $2 $3
+done
+python summarize.py ../results-sklearn/iwpc/membership/unknown-test-error SGDReg
+python summarize.py ../results-sklearn/iwpc/membership/known-test-error SGDReg
